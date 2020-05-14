fdf669b1b7c9f9363b64ba84ea2a96096bf1d67c
alex-bagnall <Niscowal@gmail.com>
2020-05-14 21:33:18 +0100

    Final version.

---
+++ TaskList/app/src/main/java/com/bagnall/tasklist/TaskContract.java
@@ -0,0 +1,18 @@
+package com.bagnall.tasklist;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.bagnall.tasklist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+
+        public static final String COL_TASK_REMINDER_INTERVAL = "interval";
+
+        public static final String COL_TASK_NEXT_REMINDER = "next";
+    }
+}
